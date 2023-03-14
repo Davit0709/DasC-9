d19444e22b7897b6d8dfdf08d750ffabc651502b
Davit <dav0709@icloud.com>
2023-03-15 00:04:45 +0400

    uxagrum

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите размерность N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        double[] point1 = new double[N];
+        Console.WriteLine("Введите координаты первой точки:");
+        for (int i = 0; i < N; i++) {
+            Console.Write("координата {0}: ", i + 1);
+            point1[i] = double.Parse(Console.ReadLine());
+        }
+
+        double[] point2 = new double[N];
+        Console.WriteLine("Введите координаты второй точки:");
+        for (int i = 0; i < N; i++) {
+            Console.Write("координата {0}: ", i + 1);
+            point2[i] = double.Parse(Console.ReadLine());
+        }
+
+        double sumOfSquares = 0;
+        for (int i = 0; i < N; i++) {
+            double diff = point1[i] - point2[i];
+            sumOfSquares += diff * diff;
+        }
+        double distance = Math.Sqrt(sumOfSquares);
+
+        Console.WriteLine("Расстояние между точками: {0}", distance);
+    }
+}
+
